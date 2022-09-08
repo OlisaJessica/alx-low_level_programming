fc2f28fbc53ed9e5492d0dd749e6ed5f425f330e
OlisaJessica <111019191+OlisaJessica@users.noreply.github.com>
2022-09-08 14:40:01 -0700

    Hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+#include <string.h>
+
+/**
+ * main -printing
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+
+	puts("Programming is like building a multiligual puzzle"\n);
+	return (0);
+}
