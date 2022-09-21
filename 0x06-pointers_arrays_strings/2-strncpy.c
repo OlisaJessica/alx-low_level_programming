e3fbb2f4dcc9b1f9d072bee0810783044aadc3cc
OlisaJessica <111019191+OlisaJessica@users.noreply.github.com>
2022-09-21 15:15:55 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -6,12 +6,14 @@
  * @n: The maximum number of bytes to copied from src.                                                                               
  * Return: A pointer to the resulting string dest.                                                                                   
  */                                                                                                                                  
-char *_strncpy(char *dest, char *src, int n)                                 {                                                                                    int index = 0, src_len = 0;                                                                                                 
+char *_strncpy(char *dest, char *src, int n)                                 {                                                                            
+
+	int index = 0, src_len = 0;                                                                                                 
          while (src[index++])                                                             src_len++;                                                                                                             
         for (index = 0; src[index] && index < n; index++)                                                                            
                 dest[index] = src[index];                                                                                             
         for (index = src_len; index < n; index++)                                                                                    
                 dest[index] = '\0';                                                                                                              
         return (dest);                           
-
+}
 
