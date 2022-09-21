e26687b92354a3fcb5f0596617541298fd3926f8
OlisaJessica <111019191+OlisaJessica@users.noreply.github.com>
2022-09-21 14:37:35 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,18 +9,20 @@
  * Return: A pointer ro the destination string @dest.
  */
 
-char *strcat(char *dest, const char *src)
+char *_strcat(char *dest, char *src)
 {
-	int index = 0, dest_len = 0;
+	int len = 0, i;
 
-	while (dest[index++1]
-			dest_len++;
+	while (dest[len])
+          len++
 
 
-
-	for (index = 0; src[index]; index++)
-	   dest[dest_len++] =src[index];
-
-	   return (dest);
+	for (i = 0; src[i] != '\0'; i++)
+	{
+	   dest[len] =src[i];
+	   len+= 1;
+	 }
+	dest[len] ='\0';
+         return (dest);
 }
 
