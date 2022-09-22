957f63af1e44c835ab3178bea1cf9a850422c099
OlisaJessica <111019191+OlisaJessica@users.noreply.github.com>
2022-09-22 13:55:34 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,27 +1,24 @@
 #include "main.h"
-#include <string.h>
 
 /**
- * strcat - concatenates the string pointed to by @src, including the terminat
- *           null bytes to the end of the string pointed to by @dest.
- * @dest: A pointer to the string to be concatenated upon.
- * @src: The source string to be appended to @dest
+ *_strcat - concatenates  the string pointed to by @src to
+ * the end of the string pointed to by @dest
+ *@dest: String that will be appended
+ *@src: String to be concatenated upon
  *
- * Return: A pointer ro the destination string @dest.
+ * Return: returns poiner to @dest
  */
 
 char *_strcat(char *dest, char *src)
 {
-	int len = 0, i;
 
-	while (dest[len])
-          len++
-        for (i = 0; src[i] != '\0'; i++)
-	{
-	   dest[len] =src[i];
-	   len+= 1;
-	 }
-	dest[len] ='\0';
-         return (dest);
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
+
+	return (dest);
 }
-
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,23 @@
-#ifndef MAIN_H                                                                    
-#define MAIN_H                                                                                                                                                                      
-                                                                                                       int _putchar(char c);                         
+#ifndef MAIN_H
+#define MAIN_H
+#include <srdio.h>
+/**
+ * void prototypes()
+ * int prototypes()
+ * char prototype()
+ */
+void reverse_array(int*a, int n);
+void print_number(int n);
+void print_buffer(char*b, int size);
+int_strcmp(char*s1, char*s2);
+char*strcat(char*dest, char*src);
+char*_strncat(char*dest, char*src, int n);
+char*_strncpy(char*dest, char*src, int n);
+char*string_toupper(char*);
+char*cap_string(char*);
+char*leet(char*);
+char*rot13(char*);
+char*infinite_add(char*n1, char*n2, char*r, int size_r);
+char*add_strings(char*n1, char*n2, char*r, int r_index);
 
-char *_strcat(char *dest, char *src);                                                                                                                                                                                                                                                                                                                
-#endif
+#endif /*MAIN_H*/
