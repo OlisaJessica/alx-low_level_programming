224d6971a0f9be78572e6abe1f8ff3a89902234d
OlisaJessica <111019191+OlisaJessica@users.noreply.github.com>
2022-09-27 04:28:41 -0700

    strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -22,4 +22,4 @@ char *_strpbrk(char *s, char *accept)
 			return (s + i);
 	}
 	return (0);
-}                                                                                                                                                                                                              
+}
