8726618c7bfdbe096ef914388f19f612850cbe97
OlisaJessica <111019191+OlisaJessica@users.noreply.github.com>
2022-09-27 02:44:28 -0700

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _*strchr - locates a charachter in a string.
+ * @s: The string to be searched.
+ * @c: The charachter to be located.
+ *
+ * Return: if c is found  a pointer to the first occurence.
+ *   if c is not found - NULL 
+ */
+char *_strchr(char *s, char c)
+{
+	int index;
+
+	for (index = 0; s[index] >= '\0', index++)
+	{
+		if (s[index] == c)
+			return(s + index);
+	}
+	 
+	 return ('\0');
+}
