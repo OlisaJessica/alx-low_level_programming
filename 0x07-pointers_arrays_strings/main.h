747fc39ceb5ac9f83d6dc6724742c755222cdb06
OlisaJessica <111019191+OlisaJessica@users.noreply.github.com>
2022-09-27 02:03:15 -0700

    memcpy

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,15 +1,15 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stddef.h>
-
 /**
  * void prototype()
  * int prototype()
  */
 
+#include <stddef.h>
+
 int _putchar(char c);
-char *_memset(char *s, char b, unsigned int n);
+void *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src,unsigned int n);
 char *_stchr(char *s, char c);
 unsigned int_strspn(char *s, char c);
@@ -19,4 +19,4 @@ void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
                              
-#endif /*MAIN_H*/
+#endif
