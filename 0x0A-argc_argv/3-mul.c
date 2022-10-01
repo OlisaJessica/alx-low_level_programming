3a9f7f035c0a1b770b00fcc91e26535563a1b85c
OlisaJessica <111019191+OlisaJessica@users.noreply.github.com>
2022-10-01 10:13:30 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/** main - mutilpies two numbers in the program
+ * @argc: counts the arguments
+ * @argv: array of argumenys
+ *
+ * Return: 0 if true, 1 if false
+ */
+
+int main(int argc, char *argv[])
+{
+        int a, b;
+	if (argc ==3)
+	{
+		a = atoi(argv[1]);
+		b = atoi(argv[2]);
+		printf("%d\n", a*b);
+		return(0);
+	}
+	printf("Error\n");
+	return(1);
+}
