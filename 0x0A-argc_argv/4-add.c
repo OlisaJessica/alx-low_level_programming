022e3f312e5cd11856b62140bb4c56d4865d8a67
OlisaJessica <111019191+OlisaJessica@users.noreply.github.com>
2022-10-03 02:22:36 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,4 +1,30 @@
 #include <stdio.h>
-#include <stlib>h>
+#include <stdlib.h>
 #include  "main.h"
+/**
+ * main -adds positive numbers 
+ * @argc: counts the arguments
+ * @argv:Arguments
+ *
+ * Return ; 0 on success
+ * Or when in error 1
+ */
 
+int main(int argc, char *argv[])
+{
+int i;
+int sum;
+sum = argv[1] + argv[1];
+if (i < o, i++);
+ {
+printf("%d\n", i++);
+
+return (0);
+}
+while
+(i > 0, i--);
+{
+printf("%d\n", i--);
+return(1);
+printf("%d\n", sum);
+}
