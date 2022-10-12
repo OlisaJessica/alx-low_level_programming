136fb84f0c1c1c45cae8962d49ce149ce5d32c23
OlisaJessica <111019191+OlisaJessica@users.noreply.github.com>
2022-10-12 13:31:11 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -6,7 +6,8 @@ int _strlen(char *str);
 char *_strcopy(char *dest, char *src);
 dog_t *new_dog(char *name, float age, char *owner);
 
-/** strlen -finds the length of a string.
+/**
+ * strlen -finds the length of a string.
  * @str: the string to be measured
  *
  *Return: The length of the string.
@@ -14,15 +15,15 @@ dog_t *new_dog(char *name, float age, char *owner);
 int _strlen(char *str)
 {
 	int len = 0;
+
 	while (*str++)
 		len++;
 	return (len);
 }
 
 /**
- * _strcopy _copies a string pointed to by src, includung the
-             terminating nullbyte to a buffer pointed to by dest
- *@dest: the buffer stering the string copy
+ * _strcopy _copies a string pointed to by src,
+ *  @dest: the buffer stering the string copy
  *@src: the source string.
  *
  * Return: The pointer to dest.
@@ -30,8 +31,10 @@ int _strlen(char *str)
 char *_strcopy(char *dest, char *src)
 {
 	int index = 0;
+
 	for (index = 0; src[index]; index++)
-			dest[index] = src[index];
+	et number
+		dest[index] = src[index];
 	dest[index] = '\0';
 	return (dest);
 }
@@ -53,12 +56,12 @@ doggo = malloc(sizeof(dog_t));
 if (doggo == NULL)
 return (NULL);
 doggo->name = malloc(sizeof(char) * (_strlen(name) + 1));
-if (doggo->name ==NULL)
+if (doggo->name == NULL)
 {
 free(doggo);
-return(NULL);
+return (NULL);
 }
-doggo->owner = malloc(sizeof(char) * (_strlen(owner) +1));
+doggo->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
 if (doggo->owner == NULL)
 {
 free(doggo->name);
