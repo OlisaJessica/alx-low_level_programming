e1720e20404d6226eaa1347464748284fd0411a4
OlisaJessica <111019191+OlisaJessica@users.noreply.github.com>
2022-10-26 14:42:44 -0700

    2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -16,57 +16,32 @@
 list_t *add_node(list_t **head, const char *str)
 
 {
-
 	char *duplicate;
 
 	list_t *new;
 
 	int len;
 
-
-
 	new = malloc(sizeof(list_t));
-
-
 
 	if (new == NULL)
 
 		return (NULL);
-
-
-
 	duplicate = strdup(str);
 
-
-
 	if (duplicate == NULL)
-
 	{
-
 		free(new);
 
 		return (NULL);
-
 	}
-
-
-
 	for (len = 0; str[len];)
 
 		len++;
+		new->str = duplicate;
 
-
-
-	new->str = duplicate;
-
-	new->len = len;
-
-	new->next = *head;
-
-
-
-	*head = new;
-
-	return (new);
-
+new->len = len;
+new->next = *head;
+*head = new;
+return (new);
 }
